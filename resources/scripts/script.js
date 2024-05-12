5e3f04f9dad30ecdfd88b03e81d5549b615f5590
tenmongit <148588521+tenmongit@users.noreply.github.com>
2024-05-12 20:19:30 +0500

    finish home page

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,36 +1,42 @@
 const products = [
-    { name: 'Black T-Shirt', 
+    { name: 'Black T-Shirt ', 
     price: '15.99$',
+    salePrice: '10.99$',
     imageUrl: 'resources/images/black-t-shirt.jpg',  
     imageAlt: 'black t-shirt image', 
     description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
     },
-    { name: 'White T-Shirt', 
+    { name: 'White T-Shirt ', 
     price: '15.99$', 
+    salePrice: '10.99$', 
     imageUrl: 'resources/images/white-t-shirt.jpg', 
     imageAlt: 'white t-shirt image',
     description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
     },
-    { name: 'Black Regular-Fit Jeans', 
-    price: '15.99$', 
+    { name: 'Black Regular-Fit Jeans ', 
+    price: '25.99$', 
+    salePrice: '21.99$',
     imageUrl: 'resources/images/black-regular-fit-jeans.jpg', 
     imageAlt: 'black Regular-Fit Jeans image',
     description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
     },
-    { name: 'Gray Shirt', 
-    price: '15.99$', 
+    { name: 'Gray Shirt ', 
+    price: '22.99$', 
+    salePrice: '17.99$',
     imageUrl: 'resources/images/gray-shirt.jpg',  
     imageAlt: 'gray shirt image',
     description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
     },
-    { name: 'White Fedora', 
-    price: '15.99$', 
+    { name: 'White Fedora ', 
+    price: '12.99$', 
+    salePrice: '8.99$',
     imageUrl: 'resources/images/white-fedora.jpg', 
     imageAlt: 'white fedora image',
     description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
     },
-    { name: 'Black Tie', 
-    price: '15.99$', 
+    { name: 'Black Tie ', 
+    price: '6.99$', 
+    salePrice: '3.99$',
     imageUrl: 'resources/images/black-tie.jpg', 
     imageAlt: 'black tie image',
     description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
@@ -47,10 +53,10 @@ function generateProductCards(products) {
         card.innerHTML = `
             <div class="col">
                 <div class="card h-100" style="width: 18rem;">
-                    <img src="${product.imageUrl}" class="card-img-top" alt="black t shirt image">
+                    <img src="${product.imageUrl}" class="card-img-top" alt="${product.imageAlt}" style="width: 100%; height: 400px;">
                     <div class="card-body">
-                        <h5 class="card-title">${product.name}<span class="badge text-bg-danger">SALE!</span></h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">${product.price}</h6>
+                        <h5 class="card-title">${product.name}<span class="badge text-bg-danger">SALE!</span> <strong>${product.salePrice}</strong></h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary"><s>${product.price}</s></h6>
                         <p class="card-text">${product.description}</p>
                             <div class="card-footer">
                                 <button class="btn btn-primary" onclick="addToCart()"> Add to Cart</button>
@@ -61,7 +67,7 @@ function generateProductCards(products) {
             </div>
         `;
 
-        if (product.price.includes('$') && product.price.split('$')[0] < 20) {
+        if (product.salePrice.includes('$') && product.salePrice.split('$')[0] < 20) {
             container.appendChild(card);
         }
     });
